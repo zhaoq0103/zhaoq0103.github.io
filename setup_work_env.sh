89acdb14d884257cb4dbd995818f1abec3b44744
zhaoq0103 <zhaoq0103@zhaoq0103-Mac-mini.local>
2023-05-16 18:28:35 +0800

    服务端架构之LBS和存储

---
+++ setup_work_env.sh
@@ -0,0 +1,18 @@
+# 1. 输入法
+# 2. chrome
+# 3. weixin_work from appstore
+# 4. youdaoyun
+# 5. wps
+
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.di diff
+
+# git终端中文显示问题
+git config --global core.quotepath false
+
+# 确认一下显示中文是否正常
+# git log --stat
